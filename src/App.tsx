6318703033bbbdea8a8e83ded69f7d0a10dfc3bb
Railimaa <ferreiralimarai0@gmail.com>
2023-10-30 00:59:23 -0300

    chore: finished setup for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
-import { useCallback, useState } from 'react';
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  const print = useCallback(() => {
-    console.log(count);
-  }, [count]);
-
-  print();
-
   return (
     <div>
       <h3>Hello World</h3>
-      <button
-        type="button"
-        onClick={() => setCount((prevState) => prevState + 1)}
-      >
-        Clique {count}
-      </button>
     </div>
   );
 }
