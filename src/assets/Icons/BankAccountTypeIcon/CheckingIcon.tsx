4a96e2a1cbf7e8bb37c6687a8b772a220aa0d17d
Railimaa <ferreiralimarai0@gmail.com>
2023-11-08 15:07:49 -0300

    feat: add Icons Svgs

--- src/assets/Icons/BankAccountTypeIcon/CheckingIcon.tsx
+++ src/assets/Icons/BankAccountTypeIcon/CheckingIcon.tsx
@@ -9,30 +9,23 @@ export function CheckingIcon() {
     >
       <rect x="1" y="1" width="42" height="42" rx="21" fill="#E9ECEF" />
       <path
-        d="M16.1884 14.9492H27.8116C29.777 14.9492 31 16.3367 31 18.3001V25.6986C31 27.6621 29.777 29.0495 27.8106 29.0495H16.1884C14.223 29.0495 13 27.6621 13 25.6986V18.3001C13 16.3367 14.2289 14.9492 16.1884 14.9492Z"
-        stroke="#495057"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M16.6641 21.2988V22.7019"
-        stroke="#495057"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M27.3359 22.7019V21.2988"
-        stroke="#495057"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
         fillRule="evenodd"
         clipRule="evenodd"
-        d="M24.2868 22.0003C24.2868 20.7365 23.2623 19.7129 21.9994 19.7129C20.7355 19.7129 19.7109 20.7365 19.7109 22.0003C19.7109 23.2642 20.7355 24.2878 21.9994 24.2878C23.2623 24.2878 24.2868 23.2642 24.2868 22.0003Z"
+        d="M27.0351 14.5391H16.9639C14.5276 14.5391 13 16.2641 13 18.7053V25.2933C13 27.7355 14.5198 29.4596 16.9639 29.4596H27.0342C29.4792 29.4596 31 27.7355 31 25.2933V18.7053C31 16.2641 29.4792 14.5391 27.0351 14.5391Z"
+        stroke="#495057"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M13 19.8341H31"
+        stroke="#495057"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M17.1768 25.1701H20.1453"
         stroke="#495057"
         strokeWidth="1.5"
         strokeLinecap="round"
