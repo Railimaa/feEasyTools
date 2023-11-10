e3acfe6ff0da59ea81d18e8aac4908a5f49d42f1
Railimaa <ferreiralimarai0@gmail.com>
2023-11-09 21:24:18 -0300

    feat: add Fab

---
+++ src/pages/YourFinances/components/Fab/index.tsx
@@ -0,0 +1,36 @@
+import { PlusIcon } from '@radix-ui/react-icons';
+
+import { BankAccountIcon } from '../../../../assets/Icons/BankAccountIcon';
+import { CategoryIcon } from '../../../../assets/Icons/categories/CategoryIcon';
+import { DropdownMenu } from '../../../../components/DropdownMenu';
+
+import { Container } from './style';
+
+export function Fab() {
+  return (
+    <Container>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <PlusIcon width={24} height={24} color="#fff" />
+        </DropdownMenu.Trigger>
+
+        <DropdownMenu.Content side="top">
+          <DropdownMenu.Item colorHover="#E5DBFF" style={{ gap: '8px' }}>
+            <CategoryIcon type="income" />
+            Nova Receita
+          </DropdownMenu.Item>
+
+          <DropdownMenu.Item colorHover="#E5DBFF" style={{ gap: '8px' }}>
+            <CategoryIcon type="expense" />
+            Nova Despesa
+          </DropdownMenu.Item>
+
+          <DropdownMenu.Item colorHover="#E5DBFF" style={{ gap: '8px' }}>
+            <BankAccountIcon />
+            Nova Conta
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Container>
+  );
+}
