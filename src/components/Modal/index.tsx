2b0db24878cb84dfc9480edfbcb6da03aca66ba9
Railimaa <ferreiralimarai0@gmail.com>
2023-11-09 23:06:12 -0300

    feat: add component modal

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/require-default-props */
+/* eslint-disable import/no-extraneous-dependencies */
+import * as RdxDialog from '@radix-ui/react-dialog';
+import { Cross2Icon } from '@radix-ui/react-icons';
+import React from 'react';
+
+import { Container } from './style';
+
+interface IModalProps {
+  open: boolean;
+  title: string;
+  children: React.ReactNode;
+  onClose?: () => void;
+  rightAction?: React.ReactNode;
+}
+
+export function Modal({
+  open,
+  title,
+  children,
+  onClose,
+  rightAction,
+}: IModalProps) {
+  return (
+    <RdxDialog.Root open={open} onOpenChange={onClose}>
+      <RdxDialog.Portal>
+        <Container>
+          <RdxDialog.Overlay className="overlay" />
+
+          <RdxDialog.Content className="content">
+            <header>
+              <button type="button">
+                <Cross2Icon width={24} height={24} />
+              </button>
+
+              <strong>{title}</strong>
+
+              <div className="rightAction">{rightAction}</div>
+            </header>
+
+            <div className="main">{children}</div>
+          </RdxDialog.Content>
+        </Container>
+      </RdxDialog.Portal>
+    </RdxDialog.Root>
+  );
+}
---
+++ src/components/Modal/style.ts
@@ -0,0 +1,64 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  .overlay {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: rgba(0, 0, 0, 0.8);
+    backdrop-filter: blur(4px);
+    z-index: 401;
+  }
+
+  .content {
+    width: 100%;
+    max-width: 400px;
+    padding: 24px;
+    border-radius: 16px;
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    background: #fff;
+    box-shadow: 0px 11px 20px 0px rgba(0, 0, 0, 0.1);
+    z-index: 402;
+    outline: none;
+
+    header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      height: 48px;
+
+      button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: none;
+        background: transparent;
+        outline: none;
+        width: 48px;
+        height: 48px;
+      }
+
+      strong {
+        font-size: 18px;
+        letter-spacing: -1px;
+        font-weight: 700;
+        color: #343a40;
+      }
+
+      .rightAction {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+    }
+
+    .main {
+      margin-top: 40px;
+    }
+  }
+`;
