5a2452153028a536a57c8b042c4e2009301813e4
Railimaa <ferreiralimarai0@gmail.com>
2023-11-13 22:39:09 -0300

    feat: create service update account

---
+++ src/services/bankAccountService/update.ts
@@ -0,0 +1,15 @@
+import { httpClient } from '../httpClient';
+
+interface IUpdateBankAccount {
+  id: string;
+  initialBalance: number;
+  name: string;
+  color: string;
+  type: 'CHECKING' | 'INVESTMENT' | 'CASH';
+}
+
+export async function update({ id, ...body }: IUpdateBankAccount) {
+  const { data } = await httpClient.put(`/bank-accounts/${id}`, body);
+
+  return data;
+}
