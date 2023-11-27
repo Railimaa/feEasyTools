ebbbe2c14eaf54c91da864a384169d49ba299a04
Railimaa <ferreiralimarai0@gmail.com>
2023-11-27 14:55:46 -0300

    feat: create service categoryTransaction delete

--- src/services/categoriesTransactionService/index.ts
+++ src/services/categoriesTransactionService/index.ts
@@ -1,4 +1,5 @@
 import { create } from './create';
+import { deleted } from './delete';
 import { getAll } from './getAll';
 import { update } from './update';
 
@@ -6,4 +7,5 @@ export const categoriesTransactionService = {
   getAll,
   create,
   update,
+  deleted,
 };
